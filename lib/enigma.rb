a1209c207cb60b79f92651c619a0f3c400ebc8e4
Cam C <ckc.cameron@gmail.com>
2020-06-04 17:13:38 -0600

    Creates enigma class and tests it exists

---
+++ lib/enigma.rb
@@ -0,0 +1,10 @@
+class Enigma
+
+  def encrypt(message, key, date)
+    #hash with 3 keys - all strings
+  end
+
+  def decrypt(ciphertext, key, date)
+    #hash with 3 keys - all strings
+  end
+end
