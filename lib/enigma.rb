e8cdb02f71c94504e49994cf7a970f714ca96dfc
Cam C <ckc.cameron@gmail.com>
2020-06-06 16:25:58 -0600

    Initializes Enigma class with message, key, date and shift attributes; all tests passing

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,18 @@
+require './lib/shift'
+require 'date'
+
 class Enigma
+  attr_reader :message,
+              :key,
+              :date,
+              :shift
+
+  def initialize(message, key, date)
+    @message = message
+    @key = key
+    @date = date
+    @shift = Shift.new(@key, @date)
+  end
 
   def encrypt(message, key, date)
     #hash with 3 keys - all strings
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,11 +1,20 @@
 require './test/test_helper'
-# require './test/date'
+require 'date'
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
   def test_it_exists
-    enigma = Enigma.new
+    enigma = Enigma.new("hello world", "02715", "040895")
 
     assert_instance_of Enigma, enigma
+  end
+
+  def test_it_has_readable_attributes
+    enigma = Enigma.new("hello world", "02715", "040895")
+
+    assert_equal "hello world", enigma.message
+    assert_equal "02715", enigma.key
+    assert_equal "040895", enigma.date
+    assert_instance_of Shift, enigma.shift
   end
 end
