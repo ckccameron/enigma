7497844afb567c6ff1a258567c8cecac210482a1
Cam C <ckc.cameron@gmail.com>
2020-06-06 22:24:31 -0600

    Uses different approach to decrypt file read; decrypt CLI working

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,22 +1,45 @@
 require './lib/enigma'
-require './lib/date'
 
-message = File.open(ARGV[0]
+key = ARGV[2]
+date = ARGV[3]
 
-enigma = Enigma.new
-key =
-date = 
+handle = File.open(ARGV[0], "r")
 
-incoming_text = handle.read
+encrypted_message = handle.read
 
 handle.close
 
-puts "Created 'decrypted.txt' with the key 82648 and date 240818"
-
-text = incoming_text.downcase
-
+enigma = Enigma.new(encrypted_message, key, date)
+decrypted_message = enigma.decrypt(encrypted_message, key, date)
 writer = File.open(ARGV[1], "w")
 
-writer.write(text)
-
+writer.write(decrypted_message[:decryption])
 writer.close
+
+p "Created '#{ARGV[1]}' with the key #{decrypted_message[:key]} and date #{decrypted_message[:date]}"
+
+# input = ARGV[0]
+# output = ARGV[1]
+# key = ARGV[2].to_s
+# date = ARGV[3].to_s
+#
+# message = File.open(input)
+#
+# incoming_text = message.read
+# incoming_key = handle_2
+# incoming_date = handle_3
+#
+# handle.close
+#
+# enigma = Enigma.new(incoming_text, incoming_key, incoming_date)
+# update = enigma.decrypt(incoming_text, incoming_key, incoming_date)
+# decrypted_text = update[:decryption]
+# require "pry"; binding.pry
+#
+# puts "Created 'decrypted.txt' with the key #{incoming_key} and date #{incoming_date}"
+#
+# writer = File.open(ARGV[1], "w")
+#
+# writer.write(decrypted_text)
+#
+# writer.close
