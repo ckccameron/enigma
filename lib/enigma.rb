064039210237410fbba55bce4926dcb564b400fd
Cam C <ckc.cameron@gmail.com>
2020-06-08 09:15:36 -0600

    Revises encrypt and decrypt methods to call helper methods for shift, key, date

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -22,19 +22,21 @@ class Enigma
     Date.today.strftime("%m%d%y")
   end
 
-  def encrypt(message, key = rand(100000).to_s.rjust(5, "0"), date = Date.today.strftime("%m%d%y"))
+  def encrypt(message, key = rand_key_num, date = today_date)
+    shift = Shift.new(key, date)
     encryption_hash = Hash.new
-    encryption_hash[:encryption] = @shift.encrypt(@message)
-    encryption_hash[:key] = @key
-    encryption_hash[:date] = @date
+    encryption_hash[:encryption] = shift.encrypt(message)
+    encryption_hash[:key] = key
+    encryption_hash[:date] = date
     encryption_hash
   end
 
-  def decrypt(message, key = rand(100000).to_s.rjust(5, "0"), date = Date.today.strftime("%m%d%y"))
+  def decrypt(message, key = rand_key_num, date = today_date)
+    shift = Shift.new(key, date)
     decryption_hash = Hash.new
-    decryption_hash[:decryption] = @shift.decrypt(@message)
-    decryption_hash[:key] = @key
-    decryption_hash[:date] = @date
+    decryption_hash[:decryption] = shift.decrypt(message)
+    decryption_hash[:key] = key
+    decryption_hash[:date] = date
     decryption_hash
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -10,16 +10,6 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, enigma
   end
 
-  # def test_it_has_readable_attributes
-  #   # enigma = Enigma.new("hello world", "02715", "040895")
-  #   enigma = Enigma.new
-  #
-  #   assert_equal "hello world", enigma.message
-  #   assert_equal "02715", enigma.key
-  #   assert_equal "040895", enigma.date
-  #   assert_instance_of Shift, enigma.shift
-  # end
-
   def test_it_can_produce_random_number_for_key
     enigma = Enigma.new
 
@@ -33,47 +23,32 @@ class EnigmaTest < Minitest::Test
 
     assert_equal 6, enigma.today_date.length
   end
+  
+  def test_it_can_encrypt
+    # enigma = Enigma.new("hello world", "02715", "040895")
+    enigma = Enigma.new
 
-#   def test_it_has_today_set_as_default_date
-#     # enigma = Enigma.new("hello world", "02715")
-#     enigma = Enigma.new
-#
-#     assert_equal 6, enigma.date.length
-#     assert_equal "20", enigma.date[-2..-1]
-#   end
-#
-#   def test_it_has_random_key_as_default
-#     # enigma = Enigma.new("hello world")
-#     enigma = Enigma.new
-#
-#     assert_equal 5, enigma.key.length
-#   end
-#
-#   def test_it_can_encrypt
-#     # enigma = Enigma.new("hello world", "02715", "040895")
-#     enigma = Enigma.new
-#
-#     expected =
-#     {
-#       encryption: "keder ohulw",
-#       key: "02715",
-#       date: "040895"
-#     }
-#
-#     assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
-#   end
-#
-#   def test_it_can_decrypt
-#     # enigma = Enigma.new("keder ohulw", "02715", "040895")
-#     enigma = Enigma.new
-#
-#     expected =
-#     {
-#       decryption: "hello world",
-#       key: "02715",
-#       date: "040895"
-#     }
-#
-#     assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
-#   end
+    expected =
+    {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_it_can_decrypt
+    # enigma = Enigma.new("keder ohulw", "02715", "040895")
+    enigma = Enigma.new
+
+    expected =
+    {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
+  end
 end
